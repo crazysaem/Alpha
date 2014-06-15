c32a5de292b0c7aa998e4a3d539e8de80fe6660a
crazysaem <crazysaem@gmail.com>
2014-06-15 17:53:25 +0200

    fixed camera unwanted angle detection

--- core/src/com/crazysaem/alpha/graphics/CameraController.java
+++ core/src/com/crazysaem/alpha/graphics/CameraController.java
@@ -11,7 +11,8 @@ public class CameraController extends CameraInputController
 {
   private final Vector3 tmpV = new Vector3();
 
-  private Vector3 position = new Vector3();
+  private Vector3 camPosition = new Vector3();
+  private Vector3 camPositionXZProjected = new Vector3();
 
   public CameraController(Camera camera)
   {
@@ -24,13 +25,24 @@ public class CameraController extends CameraInputController
     if (button == rotateButton) {
       tmpV.set(camera.direction).crs(camera.up).y = 0f;
 
-      position.y = camera.position.y;
-      position.z = camera.position.z;
       deltaY *= rotateAngle;
-      float currentAngle = (float)Math.acos(Vector3.Z.dot(position.nor())) * 57.3f;
-      if (currentAngle - deltaY > 5.0f)
+      if (deltaY > 0)
+      {
+        camPosition.x = camera.position.x;
+        camPosition.y = camera.position.y;
+        camPosition.z = camera.position.z;
+        camPosition.nor();
+        camPositionXZProjected.x = camera.position.x;
+        camPositionXZProjected.z = camera.position.z;
+        camPositionXZProjected.nor();
+        float currentAngle = (float) Math.acos(camPosition.dot(camPositionXZProjected)) * 57.3f;
+        if (currentAngle - deltaY > 5.0f)
+          camera.rotateAround(target, tmpV.nor(), deltaY);
+      }
+      else
+      {
         camera.rotateAround(target, tmpV.nor(), deltaY);
-
+      }
       camera.rotateAround(target, Vector3.Y, deltaX * -rotateAngle);
     } else if (button == translateButton) {
       camera.translate(tmpV.set(camera.direction).crs(camera.up).nor().scl(-deltaX * translateUnits));
