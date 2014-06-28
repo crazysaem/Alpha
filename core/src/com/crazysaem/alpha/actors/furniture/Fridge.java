4941efe323ee73954fdb9944594f9c5608b8e4b5
crazysaem <crazysaem@gmail.com>
2014-06-28 21:54:06 +0200

    added fridge animations. next up: don't turn everything green. add fog particle system.

--- core/src/com/crazysaem/alpha/actors/furniture/Fridge.java
+++ core/src/com/crazysaem/alpha/actors/furniture/Fridge.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.attributes.FloatAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.AnimationController;
 import com.crazysaem.alpha.graphics.RenderBatch;
 import com.crazysaem.alpha.graphics.RenderUtils;
 import com.crazysaem.alpha.picking.StaticRenderable;
@@ -11,9 +12,11 @@ import com.crazysaem.alpha.picking.StaticRenderable;
 /**
  * Created by crazysaem on 24.06.2014.
  */
-public class Fridge extends StaticRenderable
+public class Fridge extends StaticRenderable implements AnimationController.AnimationListener
 {
   private Environment environment;
+  private String[] animationIDs = {"fridge_open", "fridge_tray", "fridge_tray", "fridge_freezer"};
+  private int currentAnimation = 0;
 
   public Fridge()
   {
@@ -24,21 +27,33 @@ public class Fridge extends StaticRenderable
   @Override
   protected void finishLoading()
   {
-    super.finishLoading("Fridge");
+    super.finishLoading("Fridge", "FridgeArmature");
 
     Material selectedMaterial;
     if ((selectedMaterial = RenderUtils.getMaterial(modelInstance, "Fridge")) != null)
     {
       selectedMaterial.set(new ColorAttribute(ColorAttribute.Diffuse, 192.0f / 256.0f, 237.0f / 256.0f, 97.0f / 256.0f, 1.0f));
     }
-  }
-  /*
-  @Override
-  public void render(RenderBatch renderBatch)
-  {
-    if (loading)
-      return;
 
-    renderBatch.render(modelInstance, environment);
-  }*/
+    animationController.setAnimation(animationIDs[0], 1, 1.0f, this);
+    currentAnimation = 0;
+  }
+
+  @Override
+  public void onEnd(AnimationController.AnimationDesc animation)
+  {
+    currentAnimation++;
+    currentAnimation = currentAnimation % 4;
+
+    if (currentAnimation == 2)
+      animationController.queue(animationIDs[currentAnimation], 1, -1.0f, this, 0.05f);
+    else
+      animationController.queue(animationIDs[currentAnimation], 1, 1.0f, this, 0.05f);
+  }
+
+  @Override
+  public void onLoop(AnimationController.AnimationDesc animation)
+  {
+
+  }
 }
