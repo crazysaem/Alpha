64abdb996a89592050df8c28843be8fc5db0cf14
crazysaem <crazysaem@gmail.com>
2014-10-25 17:32:14 +0200

    added copy_jars.bat file

---
+++ copy_jars.bat
@@ -0,0 +1,2 @@
+copy "C:\gdx\dist\gdx.jar" "C:\Users\crazysaem\.gradle\caches\modules-2\files-2.1\com.badlogicgames.gdx\gdx\1.4.1\df470d263438048af74860ca51fc497c16a0c76a\gdx-1.4.1.jar"
+copy "C:\gdx\dist\sources\gdx-sources.jar" "C:\Users\crazysaem\.gradle\caches\modules-2\files-2.1\com.badlogicgames.gdx\gdx\1.4.1\a36f5d3e6a3b0fa3c625f8ce88216daf99435bce\gdx-1.4.1-sources.jar"
