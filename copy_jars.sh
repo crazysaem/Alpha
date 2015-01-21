d601b050bc62837eec1849c6f08706cbdc3cc92c
crazysaem <crazysaem@gmail.com>
2015-01-21 21:21:05 +0100

    added copy_jar.sh for linux

---
+++ copy_jars.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp /home/crazysaem/workspace/libgdx/dist/gdx.jar /home/crazysaem/.gradle/caches/modules-2/files-2.1/com.badlogicgames.gdx/gdx/1.4.1/df470d263438048af74860ca51fc497c16a0c76a/gdx-1.4.1.jar
+cp /home/crazysaem/workspace/libgdx/dist/sources/gdx-sources.jar /home/crazysaem/.gradle/caches/modules-2/files-2.1/com.badlogicgames.gdx/gdx/1.4.1/a36f5d3e6a3b0fa3c625f8ce88216daf99435bce/gdx-1.4.1-sources.jar
